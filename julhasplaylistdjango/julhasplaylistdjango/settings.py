affb27da159ef4c6435a107e5001cc232ef01731
maxyee <eyaminkhan00@gmail.com>
2019-04-21 03:32:49 +0600

    added the docs code for showing the api test data

--- julhasplaylistdjango/julhasplaylistdjango/settings.py
+++ julhasplaylistdjango/julhasplaylistdjango/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -66,6 +67,17 @@ TEMPLATES = [
         },
     },
 ]
+
+#rest framework section
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
+
+
+
+
+
 
 WSGI_APPLICATION = 'julhasplaylistdjango.wsgi.application'
 
--- julhasplaylistdjango/julhasplaylistdjango/urls.py
+++ julhasplaylistdjango/julhasplaylistdjango/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+
 from rest_framework import routers
 from julhasplaylistdjango.quickstart import views
 
@@ -24,5 +25,6 @@ router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
