4edf744ff039165dfdd599b245df7c4229111ef1
maxyee <eyaminkhan00@gmail.com>
2019-04-30 05:23:40 +0600

    Finalized the code and tested all the api CRUD operations

--- playlistBackend/videos/models.py
+++ playlistBackend/videos/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-from categories.models import Categories
+#from categories.models import Categories
 
 
 class Videos(models.Model):
@@ -10,6 +10,7 @@ class Videos(models.Model):
     video_description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    categories = models.ForeignKey(Categories, related_name='categories', on_delete=models.CASCADE)
+    #categories = models.ForeignKey(Categories, related_name='categories', on_delete=models.CASCADE)
+    categories = models.CharField(max_length=50)
     user = models.ForeignKey('auth.User', related_name='videos', on_delete=models.CASCADE)
 
